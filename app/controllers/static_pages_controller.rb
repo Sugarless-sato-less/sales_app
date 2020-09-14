b7aafe7e2e00b5a6bc9399bc0d81cee5f13d3d6d
Suguru <sugar961216@gmail.com>
2020-09-14 21:08:54 +0900

    Change many

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
-  def home
-    @today_sale = Sale.where(date: Date.today)
+  include StaticPagesHelper
+  def home 
+    @today_price = some_per_time(:price)
+    @today_transactions = some_per_time(:transactions, day)
+    @today_price_total = total(@today_price)
+    @today_transactions_total = total(@today_transactions)
+    @day_price = unit(:price)
+    @total_price = total(@day_price)
+    @day_transactions = unit(:transactions)
+    @total_transactions = total(@day_transactions)
+    # binding.irb
+    @day_sale = day_total(@day_price, @day_transactions)
   end
 end
--- db/migrate/20200914064651_change_data_time_to_sales.rb
+++ db/migrate/20200914064651_change_data_time_to_sales.rb
@@ -1,6 +1,5 @@
 class ChangeDataTimeToSales < ActiveRecord::Migration[6.0]
   def change
-    
     change_column :Sales, :time, :integer
   end
 end
