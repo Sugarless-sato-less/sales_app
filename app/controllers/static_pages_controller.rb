b4eb2a44df89b19f8cb32c0ab6a01cc823c7b26d
Suguru <sugar961216@gmail.com>
2020-09-15 10:26:52 +0900

    Change home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,19 @@
 class StaticPagesController < ApplicationController
   include StaticPagesHelper
   def home 
-    @today_price = some_per_time(:price)
-    @today_transactions = some_per_time(:transactions, day)
-    @today_price_total = total(@today_price)
-    @today_transactions_total = total(@today_transactions)
-    @day_price = unit(:price)
-    @total_price = total(@day_price)
-    @day_transactions = unit(:transactions)
-    @total_transactions = total(@day_transactions)
-    # binding.irb
-    @day_sale = day_total(@day_price, @day_transactions)
+    @data_of_today = data_of_a_day("2020-9-2")
+    @sample_day = data_of_a_day(Date.yesterday)
+    
+
+    # @today_price = some_per_time(:price)
+    # @today_transactions = some_per_time(:transactions, day)
+    # @today_price_total = total(@today_price)
+    # @today_transactions_total = total(@today_transactions)
+    # @day_price = unit(:price)
+    # @total_price = total(@day_price)
+    # @day_transactions = unit(:transactions)
+    # @total_transactions = total(@day_transactions)
+    # # binding.irb
+    # @day_sale = day_total(@day_price, @day_transactions)
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,34 +1,36 @@
 module StaticPagesHelper
-  def unit(num)
-    today_sale = Sale.where(date: Date.today)
-    # @today_target = Sale.where(date: )
-    any_sale = Sale.where(date: Date.yesterday)
-    hash = {}
-    (7..22).each do |time| 
-      result = today_sale.find_by(time: time)[num] - any_sale.find_by(time: time)[num]
-      hash.store(time, result)
-    end
-    return hash
+  # def unit(num)
+  #   today_sale = Sale.where(date: Date.today)
+  #   # @today_target = Sale.where(date: )
+  #   any_sale = Sale.where(date: Date.yesterday)
+  #   hash = {}
+  #   (7..22).each do |time| 
+  #     result = today_sale.find_by(time: time)[num] - any_sale.find_by(time: time)[num]
+  #     hash.store(time, result)
+  #   end
+  #   return hash
+  # end
+
+  # def total(hash)
+  #   total = []
+  #   (7..22).each do |num|
+  #     total << hash[num]
+  #   end
+  #   return total.sum
+  # end
+
+  # def day_total(hash1, hash2)
+  #   total = []
+  #   (7..22).each do |num|
+  #     total << hash1[num] * hash2[num]
+  #   end
+  #   return total.sum
+  # end
+
+  def data_of_a_day(day = Date.today)
+    return Sale.where(date: day)
   end
-
-  def total(hash)
-    total = []
-    (7..22).each do |num|
-      total << hash[num]
-    end
-    return total.sum
-  end
-
-  def day_total(hash1, hash2)
-    total = []
-    (7..22).each do |num|
-      total << hash1[num] * hash2[num]
-    end
-    return total.sum
-  end
-
-  def 
-
+    
 end
 
 #これが今日の8時の単価出す書き方 
