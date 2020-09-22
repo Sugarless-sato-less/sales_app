3b89f5dd84e911f9444aa533fa86e7734389d6b1
Suguru <sugar961216@gmail.com>
2020-09-22 11:15:21 +0900

    Change migration

--- db/migrate/20200918065328_add_default_to_user.rb
+++ db/migrate/20200918065328_add_default_to_user.rb
@@ -1,5 +1,5 @@
 class AddDefaultToUser < ActiveRecord::Migration[6.0]
   def change
-    change_column_default :Users, :admin, dafault: false
+    change_column_default :users, :admin, dafault: false
   end
 end
