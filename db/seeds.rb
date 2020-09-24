e43cdcc1807e16b75936110aa696eaafd5771f93
Suguru <sugar961216@gmail.com>
2020-09-24 14:20:17 +0900

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-100.times do |n|
-  date = Date.new(2020,7,1) + n
+30.times do |n|
+  date = Date.new(2020,9,1) + n
   15.times do |t|
     time = 7 + t
     price = (650..1200).to_a.sample
@@ -15,20 +15,20 @@
   end
 end
 
-
-date = Date.new(2020,7,1) + n
-15.times do |t|
-  time = 7 + t
-  price = (650..1200).to_a.sample
-  transactions = (30..100).to_a.sample
-  total = price * transactions
-  Sale.create!(
-    date: date,
-    time: time,
-    price: price,
-    transactions: transactions,
-    total: total
-  )
+30.times do |n|
+  date = Date.new(2020,9,1) + n
+  15.times do |t|
+    time = 7 + t
+    price = (650..1200).to_a.sample
+    transactions = (30..100).to_a.sample
+    total = price * transactions
+    Sale.create!(
+      date: date,
+      time: time,
+      price: price,
+      transactions: transactions,
+      total: total
+    )
 end
 
 
