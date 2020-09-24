52f6cd3476d66eba827b83970edde3fb06cf87c5
Suguru <sugar961216@gmail.com>
2020-09-24 14:16:44 +0900

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
-
-
 100.times do |n|
   date = Date.new(2020,7,1) + n
-  16.times do |t|
+  15.times do |t|
     time = 7 + t
     price = (650..1200).to_a.sample
     transactions = (30..100).to_a.sample
@@ -16,6 +14,23 @@
     )
   end
 end
+
+
+date = Date.new(2020,7,1) + n
+15.times do |t|
+  time = 7 + t
+  price = (650..1200).to_a.sample
+  transactions = (30..100).to_a.sample
+  total = price * transactions
+  Sale.create!(
+    date: date,
+    time: time,
+    price: price,
+    transactions: transactions,
+    total: total
+  )
+end
+
 
 User.create!(name: "tencho",
              password: "password",
